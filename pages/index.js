ff886559f804f952907cfa3c53a77fc0e3ce58cf
Diego Garcia <diegogarcianavas.4b@gmail.com>
2022-04-04 19:10:49 +0200

    imported searchbar and footer components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import Footer from 'components/Footer/Footer'
+import SearchBar from 'components/Footer/SearchBar/SearchBar'
 import Head from 'next/head'
-import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,9 +14,11 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        {/* <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </h1> */}
+
+        <SearchBar />
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -51,19 +55,7 @@ export default function Home() {
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Footer />
     </div>
   )
 }
