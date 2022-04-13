c12a2de93f3d54b7c53edba07f115cae7574eb21
Diego Garcia <diegogarcianavas.4b@gmail.com>
2022-04-13 13:16:30 +0200

    hook to set placelholders on search bars

---
+++ components/hooks/searchPlaceholder.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react'
+
+const SEARCH_PHRASES = [
+  'Ej: Pikachu',
+  '¿Que pokemon quieres buscar hoy?',
+  'Encuentra tu pokemon favorito',
+  'Ej: fuego',
+  'Ej: Kanto',
+  '¿Donde quieres buscar tu pokemon?',
+  'Ej: Sinnoh',
+]
+
+export default function SearchPlaceholder() {
+  const [searchPhrase, setSearchPhrase] = useState(SEARCH_PHRASES[0])
+
+  useEffect(() => {
+    setSearchPhrase(
+      SEARCH_PHRASES[Math.floor(Math.random() * SEARCH_PHRASES.length)]
+    )
+  }, [])
+
+  return searchPhrase
+}
